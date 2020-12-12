71eee477aa0cd983c8ffcaf60082ccda3fe93416
Furkan <furkan.atmaca@boun.edu.tr>
2020-12-12 15:41:03 +0300

    figure-changes

--- 2-data_plot.R
+++ 2-data_plot.R
@@ -15,13 +15,13 @@ ci <- function(x){1.96*(sd(x)/sqrt(length(x)))}
 df <- readRDS("df.rds")
 df_ncr <- readRDS("df_ncr.rds")
 # recode factors for easy read
-df_ncr$difficulty2 <- dplyr::recode(df_ncr$difficulty,`1` = "Easy", `2` = "Medium", `3` = "Hard") %>% 
-  as.factor() %>% reorder.factor(new.order = c("Easy","Medium","Hard"))
+df_ncr$difficulty2 <- dplyr::recode(df_ncr$difficulty,`1` = "Easy", `2` = "Med", `3` = "Hard") %>% 
+  as.factor() %>% reorder.factor(new.order = c("Easy","Med","Hard"))
 df_correct <- readRDS("df_correct.rds")
 
-### DATA PLOTS ###
+############################## DATA PLOTS ##############################
 # how many of things there are
-df %>% dplyr::select(response_type = type, cat2, Group = group) %>%
+fig1 <- df %>% dplyr::select(response_type = type, cat2, Group = group) %>%
   group_by(cat2,response_type,Group) %>%
   ggplot(aes(response_type, fill=Group)) + 
   geom_bar(position = "dodge") +
@@ -31,7 +31,7 @@ df %>% dplyr::select(response_type = type, cat2, Group = group) %>%
   theme(text=element_text(family="Times New Roman", size=12))
 
 # mean correct responses
-df_ncr %>% group_by(Group = group, difficulty2, cat2) %>%
+fig2 <- df_ncr %>% group_by(Group = group, difficulty2, cat2) %>%
   summarise(mean_response = mean(ncr), ci = ci(ncr)) %>%
   ggplot(aes(difficulty2,mean_response, group = Group, color = Group)) + 
   geom_point() + geom_line() + 
@@ -42,19 +42,19 @@ df_ncr %>% group_by(Group = group, difficulty2, cat2) %>%
 # cumulative correct responses by time
 diff_labs <- c("Easy","Medium","Hard")
 names(diff_labs) <- c("1","2","3")
-df_correct %>% group_by(Group = group, cat2, difficulty) %>%
-  ggplot(aes(time_sec, group = Group, color = Group)) + stat_ecdf(geom = "point") +
+fig4 <- df_correct %>% group_by(Group = group, cat2, difficulty) %>%
+  ggplot(aes(time_sec, group = Group, color = Group)) + stat_ecdf(geom = "step") +
   facet_grid(cat2~difficulty, labeller = labeller(difficulty = diff_labs)) + 
   ylab("% of Data") + xlab("Time (seconds)") + 
   theme(text=element_text(family="Times New Roman", size=12))
 
 
 
-### MODEL PLOTS ###
+############################## MODEL PLOTS ##############################
 # total number of correct responses #
 ncr_model_df <- readRDS("ncr_model_df.rds") %>% head(13)
 
-ncr_model_df %>% ggplot(aes(m, y=factor(parameter, 
+fig3 <- ncr_model_df %>% ggplot(aes(m, y=factor(parameter, 
                                         levels = rev(levels(factor(parameter)))))) +
   theme(text=element_text(family="Times New Roman", size=12)) + geom_point(size = 2) +
   geom_errorbarh(aes(xmin = l, xmax = h), alpha = 1, height = 0, size = 1) +
@@ -63,9 +63,36 @@ ncr_model_df %>% ggplot(aes(m, y=factor(parameter,
 
 # number of correct responses through time course #
 time_model_df <- readRDS("time_model_df.rds")
-time_model_df %>% ggplot(aes(m, y=factor(parameter, 
+fig5 <- time_model_df %>% ggplot(aes(m, y=factor(parameter, 
                                          levels = rev(levels(factor(parameter)))))) +
   theme(text=element_text(family="Times New Roman", size=12)) + geom_point(size = 2) +
   geom_errorbarh(aes(xmin = l, xmax = h), alpha = 1, height = 0, size = 1) +
   geom_errorbarh(aes(xmin = ll, xmax = hh, height = 0)) + vline_0() +
   xlab("Estimate(log)") + ylab("Coefficients")
+
+
+############################## SAVE PLOTS ##############################
+# figure 1 save
+ggsave("VF-Fig_1.pdf", plot = fig1, width = 6, height = 6, device = cairo_pdf, path = "./VF-figures")
+ggsave("VF-Fig_1.tiff", plot = fig1, width = 6, height = 6, path = "./VF-figures")
+ggsave("VF-Fig_1.png", plot = fig1, width = 6, height = 6, path = "./VF-figures")
+
+# figure 2 save
+ggsave("VF-Fig_2.pdf", plot = fig2, width = 5, height = 2.5, device = cairo_pdf, path = "./VF-figures")
+ggsave("VF-Fig_2.tiff", plot = fig2, width = 5, height = 2.5, path = "./VF-figures")
+ggsave("VF-Fig_2.png", plot = fig2, width = 5, height = 2.5, path = "./VF-figures")
+
+# figure 3 save
+ggsave("VF-Fig_3.pdf", plot = fig3, width = 5, height = 4, device = cairo_pdf, path = "./VF-figures")
+ggsave("VF-Fig_3.tiff", plot = fig3, width = 5, height = 4, path = "./VF-figures")
+ggsave("VF-Fig_3.png", plot = fig3, width = 5, height = 4, path = "./VF-figures")
+
+# figure 4 save
+ggsave("VF-Fig_4.pdf", plot = fig4, width = 6, height = 4, device = cairo_pdf, path = "./VF-figures")
+ggsave("VF-Fig_4.tiff", plot = fig4, width = 6, height = 4, path = "./VF-figures")
+ggsave("VF-Fig_4.png", plot = fig4, width = 6, height = 4, path = "./VF-figures")
+
+# figure 5 save
+ggsave("VF-Fig_5.pdf", plot = fig5, width = 5, height = 4, device = cairo_pdf, path = "./VF-figures")
+ggsave("VF-Fig_5.tiff", plot = fig5, width = 5, height = 4, path = "./VF-figures")
+ggsave("VF-Fig_5.png", plot = fig5, width = 5, height = 4, path = "./VF-figures")
